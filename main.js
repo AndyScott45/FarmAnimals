ee214ed469edf9c5148f50e9e7bb14863a440c3f
Andy Biira <biiraledum@gmail.com>
2020-07-28 19:52:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ btn.onclick=function(){ // adds an onclick event listener to the btn element
 const choice=input.value.toLowerCase(); // stores reference to the input text field in lowercase in a constant called choice
 
 if (choice===animals[0].toLowerCase()){ // adds a conditional to determine if choice is equivalent to first animal name then run the code below
-window.open("https://github.com/AndyScott45/FarmAnimals/blob/master/lions.html", "_blank");
+window.open("https://andyscott45.github.io/wildlife/", "_blank");
 } else if (choice === animals[1].toLowerCase()) { // if choice is equivalent to the second animal name, run the code below
 window.open ("http://programminghead.com");
 } else{ // if it is not equivalent to any of the above run the code below
